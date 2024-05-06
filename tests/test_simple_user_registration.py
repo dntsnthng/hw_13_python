441f62031775e4ff26cf31aa305eb3719637448a
Elmin Gulamov <g.elmn@yandex.ru>
2024-05-06 15:53:17 +0400

    Initial commit

---
+++ tests/test_simple_user_registration.py
@@ -0,0 +1,19 @@
+import allure
+
+from demoqa_tests.data.users import user
+from demoqa_tests.model.pages.registration_page import RegistrationPage
+
+
+@allure.tag('web')
+@allure.title('Successful fill form')
+def test_registers_user():
+    registration_page = RegistrationPage()
+
+    with allure.step('Open registration page'):
+        registration_page.open()
+
+    with allure.step('Fill in user data'):
+        registration_page.register(user)
+
+    with allure.step('Checking user data'):
+        registration_page.should_have_registered(user)
